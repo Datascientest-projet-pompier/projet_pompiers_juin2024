c1c78cf8373bea547239a5044fe314b75fd2d095
ChristelleTessier <christelle.tessier@eleve.ensai.fr>
2025-03-29 10:36:55 +0000

    maj presentation des données

--- Streamlit/fonctionsgraph.py
+++ Streamlit/fonctionsgraph.py
@@ -2,6 +2,12 @@ import streamlit as st
 
 import plotly.express as px
 import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+import plotly.graph_objects as go
+
+import statsmodels.api as sm
+from statsmodels.formula.api import ols
 
 def graph_countIncident(df, variable):
     """
@@ -11,16 +17,31 @@ def graph_countIncident(df, variable):
         df (pandas.DataFrame): Le DataFrame contenant les données.
         variable (str): Le nom de la colonne à représenter.
     """
+
+    jours_semaine = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+    mois = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
+    if variable == 'DayOfWeek':
+        df[variable] = pd.Categorical(df[variable], categories=jours_semaine, ordered=True)
+    elif variable == 'Month':
+        df[variable] = pd.Categorical(df[variable], categories=mois, ordered=True)
+
     df_count = df[['IncidentNumber', variable]].groupby(variable).agg(['count']).reset_index()
     
+    
     # Création du graphique
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=(6, 5))
     ax.bar(list(df_count[variable]), list(df_count.IncidentNumber['count']))
     ax.set_ylabel("Nombre d'incidents")
     ax.set_xlabel(variable)
     ax.set_title(f"Nombre d'incidents pour les valeurs de {variable}")
+
+    if variable == 'DayOfWeek' or variable == 'Month':
+        # Incline les annotations de l'axe des abscisses
+        plt.xticks(rotation=45, ha='right')  # Ajout de cette ligne
+        plt.tight_layout() # Ajout de cette ligne
     
-    # Affichage du graphique dans Streamlit
+        # Affichage du graphique dans Streamlit
     st.pyplot(fig)
 
 def graphQuali_countPlot(df, variable):
@@ -35,10 +56,139 @@ def graphQuali_countPlot(df, variable):
     
     # Personnaliser le layout
     fig.update_layout(
-        height=600,
-        width=800,
+        height=500,
+        width=600,
         barmode='group'  # Pour grouper les barres si nécessaire
     )
     
     # Affichage du graphique dans Streamlit
     st.plotly_chart(fig)
+
+
+def graphQuali_pointplot(df, variable):
+    """
+    Affiche un pointplot (indication de la moyenne + IC95%) des temps (transformation Box-Cox) en fonction d'une variable catégorielle.
+
+    Args:
+        df (pandas.DataFrame): Le DataFrame contenant les données.
+        variable (str): Le nom de la colonne à représenter.
+    """
+    var = 'boxcox_TotalResponseTime'  # Variable à tracer
+
+    jours_semaine = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+    mois = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
+    if variable == 'DayOfWeek':
+        df[variable] = pd.Categorical(df[variable], categories=jours_semaine, ordered=True)
+    elif variable == 'Month':
+        df[variable] = pd.Categorical(df[variable], categories=mois, ordered=True)
+    
+    # Création du graphique
+    fig, axes = plt.subplots(1, 1, figsize=(6, 5))
+    
+    # Tracer le pointplot
+    sns.pointplot(x=variable, y=var, data=df, ax=axes)
+    
+    # Titres et labels
+    axes.set_title(f"Pointplot de {var} \nen fonction de {variable}")
+    axes.set_xlabel(variable)
+    axes.set_ylabel(var)
+
+    if variable == 'DayOfWeek' or variable == 'Month':
+        # Incline les annotations de l'axe des abscisses
+        plt.xticks(rotation=45, ha='right')  # Ajout de cette ligne
+        plt.tight_layout() # Ajout de cette ligne
+    
+    # Affichage du graphique
+    plt.tight_layout()
+    st.pyplot(fig)
+
+def test_anova(df, variable):
+    """
+    Réalise un test d'ANOVA sur les 3 temps pour étudier l'influence d'une variable catégorielle sur ceux-ci.
+
+    Args:
+        df (pandas.DataFrame): Le DataFrame contenant les données.
+        variable (str): Le nom de la colonne à tester.
+    """
+    # Liste des variables sur lesquelles réaliser l'ANOVA
+    var = 'boxcox_TotalResponseTime'
+    
+    # Pour chaque variable, on réalise un test ANOVA
+    modele = var + ' ~ ' + variable
+    result = ols(modele, data=df).fit()
+        
+    # Affichage des résultats du test ANOVA
+    st.write(f"###### Test ANOVA pour étudier l'effet de {variable} sur {var}")
+    anova_result = sm.stats.anova_lm(result)
+        
+    # Affichage du résultat dans Streamlit
+    st.table(anova_result)
+    st.write("\n")
+
+def graphQuali_meanPlot(df, variable, rest):
+    """
+    Crée un pointplot (avec uniquement la moyenne) du temps total (transformation Box-Cox) en fonction d'une variable catégorielle,
+    et l'affiche dans Streamlit. Affiche un graphique combiné si le nombre de modalités est trop grand.
+
+    Args:
+        df (pandas.DataFrame): Le DataFrame contenant les données.
+        variable (str): Le nom de la colonne à représenter.
+        rest (int): Nombre de valeurs à afficher (rest/2 premières et rest/2 dernières si nécessaire).
+    """
+
+    # Calculer les moyennes du temps total par valeur de variable
+    mean = df.groupby(variable).agg({'boxcox_TotalResponseTime': 'mean'}).reset_index()
+
+    # Trier par temps total moyen par ordre décroissant
+    mean_times_by_borough = mean.sort_values(by='boxcox_TotalResponseTime', ascending=False)
+
+    def create_combined_plot(data, title, var_name):
+        half_rest = rest // 2
+        top_half = data.head(half_rest)
+        bottom_half = data.tail(half_rest)
+
+        combined_x = list(top_half[var_name]) + ['...', '...', '...'] + list(bottom_half[var_name])
+        combined_y = list(top_half['boxcox_TotalResponseTime']) + [None, None, None] + list(bottom_half['boxcox_TotalResponseTime'])
+
+        fig = go.Figure(data=go.Line(x=combined_x, y=combined_y, name='Temps total', marker_color='green'))
+
+        # Ajuster la taille pour correspondre à figsize=(6, 5)
+        fig.update_layout(title_text=title, yaxis_title="Temps moyen", height=500, width=600)
+        fig.update_xaxes(tickangle=45)
+
+        return fig
+
+    if rest >= len(df[variable].unique()):
+        title = f"Temps total moyen (transformation Box-Cox) par {variable}"
+        fig = create_combined_plot(mean_times_by_borough, title, variable)
+        st.plotly_chart(fig)
+    else:
+        title = f"Temps total moyen (transformation Box-Cox) par {variable} \n(Combiné)"
+        fig = create_combined_plot(mean_times_by_borough, title, variable)
+        st.plotly_chart(fig)
+
+
+
+def graphQuali_boxplot(df, variable):
+    """
+    Crée un boxplot de la distribution du temps total (transformation Box-Cox) en fonction d'une variable catégorielle,
+    et l'affiche dans Streamlit.
+
+    Args:
+        df (pandas.DataFrame): Le DataFrame contenant les données.
+        variable (str): Le nom de la colonne à représenter.
+    """
+    var = 'boxcox_TotalResponseTime'  # Variable à tracer
+
+    # Créer la figure
+    fig, ax = plt.subplots(figsize=(6, 5))
+
+    # Créer le boxplot
+    sns.boxplot(x=variable, y=var, data=df, ax=ax)
+    ax.set_title(f"Boxplot de {var} \nen fonction de {variable}")
+    ax.set_xlabel(variable)
+    ax.set_ylabel(var)
+
+    plt.tight_layout()
+    st.pyplot(fig)  # Afficher le graphique dans Streamlit
--- Streamlit/page_visualisation.py
+++ Streamlit/page_visualisation.py
@@ -1,11 +1,14 @@
 import streamlit as st
 import pandas as pd
 import os
+import numpy as np
 
 from fonctions import texte_justifie
 from fonctionsstat  import tab_stat
 
 import matplotlib.pyplot as plt
+
+import fonctionsgraph
 
 def visualisation():
     st.title("Visualisation")
@@ -16,13 +19,14 @@ def visualisation():
         "description de l'incident soit la variable cible (temps). L'objectif de cette partie est l'étude de ces variables"
         " pour choisir celles qui seront conservées dans le modèle.<br>"
         "A vue du grand nombre de données et après une étude préalable, pour cette partie nous n'utiliserons que les données"
-        " de 2023."
+        " de 2023.<br>"
         "Remarque : Le tableau ne contient plus de valeurs manquantes, mais les valeurs extrème ou abérantes peuvent encore être présente.")
     , unsafe_allow_html=True)
     st.markdown("<br>", unsafe_allow_html=True)
 
     # Récupération des données de 2023
     df2023 = pd.read_csv("Donnees/Doc csv/df2023.csv")
+    st.write(df2023.columns)
 
     # Création des onglets
     tab1, tab2 = st.tabs(["Etude statistique", "Data Visualisation"])
@@ -47,79 +51,200 @@ def visualisation():
         " une transformation Box-Cox pour tenter de rendre la variable le plus similaire possible à une distribution "
         "normale. De plus plus seuls certains graphiques sont présentés ici.")
         , unsafe_allow_html=True)
+      
       st.markdown("<br>", unsafe_allow_html=True)
+
       st.markdown("### Informations temporelles")
+      # Choix de l'arbre affiché
+      var_liste = ['HourOfCall','DayOfWeek','Month']
+      # Menu déroulant pour un choix unique
+      var = st.selectbox("Choisissez une variable d'intérêt :", var_liste)
 
-    st.markdown(texte_justifie(
-        "Au vue de la distribution de la variable cible (temps total), nous avons dans un premier temps effectué"
-        " une transformation Box-Cox pour tenter de rendre la variable le plus similaire possible à une distribution "
-        "normale.")
-        , unsafe_allow_html=True)
-    st.markdown("<br>", unsafe_allow_html=True)
-
-    # Création des colonnes
-    col1, col2 = st.columns(2)  # Crée deux colonnes de largeur égale
-
-    # Affichage du premier graphique dans la première colonne
-    with col1:
-        image_path = os.path.join("Donnees", "figures", "repartion_variable_HourOfCall.png")
-        st.image(image_path)
-
-    # Affichage du deuxième graphique dans la deuxième colonne
-    with col2:
-        image_path = os.path.join("Donnees", "figures", "evolution_HourOfCallboxcox_TotalResponseTime.png")
-        st.image(image_path)
-
-    st.markdown(texte_justifie(
-        "L'influence de l'heure de l'incident sur le temps de réactions est visible sur le graphique numéro 2. Ce qui"
-        " peut s'expliquer du côté métier (heure de traffic, disponibilité en heure creuse). Des influences sont aussi visibles "
-        "entre le jour et le temps, ainsi que le mois et le temps mais ne sont pas significatif (influence sur les variatipns de "
-        "l'ordre de 2% pour les jours et de l'ordre de 3% pour les mois très inférieur à 5% qui est notre seuil)")
-        , unsafe_allow_html=True)
-    st.markdown("<br>", unsafe_allow_html=True)
+      # Création des colonnes
+      col1, col2 = st.columns(2)  # Crée deux colonnes de largeur égale
+      
+      # Affichage du premier graphique dans la première colonne
+      with col1:
+        fonctionsgraph.graph_countIncident(df2023, var)
     
-    st.markdown("### Informations géographiques")
+      # Affichage du deuxième graphique dans la deuxième colonne
+      with col2:
+        fonctionsgraph.graphQuali_pointplot(df2023, var)
 
-    st.markdown(texte_justifie(
-        "Les incidents sont inégalement répartis géographiquement que ce soit par arrondissements, quartiers "
-        "(sous division d'arrondissement) ou caserne. Ces variables étant fortement corrélées nous avons décidé "
-        "de n'inclure que l'arrondissement (33 modalités) pour éviter d'inclure des variables liées et pour limiter"
-        " le temps de calcul.")
-        , unsafe_allow_html=True)
-    st.markdown("<br>", unsafe_allow_html=True)
-        
-    st.markdown(texte_justifie(
-        "Les deux graphiques ci-dessous représentent l'évolution du temps de réponse (Box-Cox) en "
-        "fonction de la distance d'une part et en fonction du ratio superficie caserne d'autre part.")
-        , unsafe_allow_html=True)
+      with st.expander(f"📌 Interprétation"):
+        st.markdown(texte_justifie(
+            "Au vue de la distribution de la variable cible (temps total), on peut observer que les variables <code>"
+            "DayOfWeek</code> et <code>Month</code> on peu d'influence sur la variables <code>boxcox_TotalResponseTime</code>.<br>"
+            "En effet pour la variable <code>DayOfWeek</code> l'écart entre le temps total moyen le plus long (le vendredi) et le plus "
+            "court (le jeudi) est de l'ordre de 2% et pour la variable <code>Month</code> l'écart entre le temps total moyen le"
+            " plus long (en novembre) et le plus court (en avril) est de l'ordre de 3%.<br>"
+            " Cette variation est inférieur à 5% pour ces deux variables elles ne seront pas conservées dans la suite de la modélisation.<br>"
+            " Pour la variable <code>HourOfCall</code> on peut constater un impact sur le temps moyen de trajet. "
+            "Le temps de trajet moyen augmente le jour, spécialement sur la tranche 10-18h, plage horaire où"
+            " le trafic est plus dense car l'activité humaine plus intense. Il y a deux pics correspondant aux plages "
+            "horaires 2-6h et 11-18h. Sur le temps total moyen, l'écart entre la valeur la plus forte (pour Hour=6h) "
+            "et la plus faible (pour Hour=22h) est de l'ordre de 6%"
+            )
+            , unsafe_allow_html=True)
+        st.markdown("<br>", unsafe_allow_html=True)
+
+        fonctionsgraph.test_anova(df2023, 'HourOfCall')
+
+      st.markdown("### Informations géographiques")
+      # Choix de l'arbre affiché
+      var_nom = ['Arrondissement', "Quartier", "Station déployée"]
+
+      # Menu déroulant pour un choix unique
+      var = st.selectbox("Choisissez une variable d'intérêt :", var_nom)
+      indice = var_nom.index(var)
+
+      var_list = ['IncGeo_BoroughName','IncGeo_WardName','DeployedFromStation_Name']
+      var = var_list[indice]
+      
+      # Création des colonnes
+      col1, col2 = st.columns(2)  # Crée deux colonnes de largeur égale
+      
+      # Affichage du premier graphique dans la première colonne
+      with col1:
+        fonctionsgraph.graphQuali_countPlot(df2023, var)
     
-    # Création des colonnes
-    col1, col2 = st.columns(2)  # Crée deux colonnes de largeur égale
+      # Affichage du deuxième graphique dans la deuxième colonne
+      with col2:
+        fonctionsgraph.graphQuali_meanPlot(df2023, var, rest=50)
 
-    # Affichage du premier graphique dans la première colonne
-    with col1:
-        image_path = os.path.join("Donnees", "figures", "evolution_distanceboxcox_TotalResponseTime2.png")
-        st.image(image_path)
+      with st.expander(f"📌 Interprétation"):
+        st.markdown(texte_justifie(
+            "Pour l'arrondissement la moyenne de TotalResponseTime varie entre 25,7 à 30,1 soit une différence de 15% environ."
+            " On peut donc supposer que l'arrondissement où a lieu l'incident a un impact sur les temps.<br>"
+            "Le dataFrame contient plus de 600 quartiers différents ; nous n'avons représenté qu'un partie"
+            " d'entre eux, correspondant aux 50 premiers/derniers pour lesquels la moyenne de TotalResponseTime"
+            " est la plus forte/faible.<br>"
+            "Pour TotalResponseTime, il y a une différence de l'ordre de 40% entre Darwin (temps moyen=36,1) et"
+            " Norbury & Pollards Hill (21,3).<br>"
+            "Les temps moyens varient aussi en fonction de la caserne de départ. La différence entre la moyenne "
+            "la plus forte et la plus faible est de l'ordre de 20% pour le temps total."
+            )
+            , unsafe_allow_html=True)
+        st.markdown("<br>", unsafe_allow_html=True)
 
-    # Affichage du deuxième graphique dans la deuxième colonne
-    with col2:
-        image_path = os.path.join("Donnees", "figures", "evolution_ratioSCboxcox_TotalResponseTime.png")
-        st.image(image_path)
-
-    st.markdown(texte_justifie(
-        "Ces deux graphiques montrent une relation avec ces deux variables ce qui s'explique par le fait"
-         " que le temps de trajet est influencé mécaniquement par la distance et qu'il est aussi influencé "
-         "par le nombre caserne proche information données par <code>ratioSC</code>")
+      st.markdown("### Variables explicatives du type d'incident")
+      st.markdown(texte_justifie(
+        "Le type d'incident est décrit par deux variables : <code>IncidentGroup</code> et <code>DetailedIncidentGroup</code> "
+        "qui donnent une information plus ou moins détaillée sur le type d'incident.")
         , unsafe_allow_html=True)
-    st.markdown("<br>", unsafe_allow_html=True)
+      
+      # Choix de l'arbre affiché
+      var_liste = ['IncidentGroup','DetailedIncidentGroup']
+      # Menu déroulant pour un choix unique
+      var = st.selectbox("Choisissez une variable d'intérêt :", var_liste)
+      
+      # Création des colonnes
+      col1, col2 = st.columns(2)  # Crée deux colonnes de largeur égale
+      
+      # Affichage du premier graphique dans la première colonne
+      with col1:
+        if var == 'IncidentGroup':
+            fonctionsgraph.graphQuali_boxplot(df2023, 'IncidentGroup')
+        else:
+            fonctionsgraph.graphQuali_countPlot(df2023, var)
+    
+      # Affichage du deuxième graphique dans la deuxième colonne
+      with col2:
+        if var == 'IncidentGroup':
+            fonctionsgraph.graphQuali_pointplot(df2023, 'IncidentGroup')
+        else:
+            fonctionsgraph.graphQuali_meanPlot(df2023, var, rest=50)
 
-    st.markdown("### Informations relationnelles caserne(déployées/responsable), arrondissement")
+      with st.expander(f"📌 Interprétation"):
+        st.write(df2023['IncidentGroup'].value_counts(normalize=True))
+        st.markdown(texte_justifie(
+            "La distribution des temps est similaire selon le type d'incident. "
+            "Sur le temps total moyen, il y a moins de 3% d'écart entre le temps le plus long (IncidentGroup=Special Service)"
+            " et le plus cours (False Alarm)<br>"
+            "La seconde variable indiquant le type d'incident : DetailedIncidentGroup est plus détaillée que IncidentGroup,"
+            " avec 27 catégories différentes (chacune n'appartenant qu'à une seule catégorie de IncidentGroup)."
+            " La répartition de DetailedIncidentGroup est hétérogène.<br>"
+            "Un peu plus de 40% des incidents sont une fausse alarme du type AFA (automatic fire alarm). "
+            "Les cinq catégories suivantes relèvent d'un autre type de fausse alarme, d'un 1er ou 2nd feu, d'une inondation"
+            " (flooding) ou d'une gestion d'entrée/sortie de personnes. La différence entre la moyenne la plus forte et la plus"
+            " faible est de l'ordre de 40% pour le temps de réaction, le temps de trajet et le temps total."
+            )
+            , unsafe_allow_html=True)
+        st.markdown("<br>", unsafe_allow_html=True)
 
-    st.markdown(texte_justifie(
-        "Ces informations sont données par les variables : <code>Bor_resp_rep</code>, <code>Bor_inc_rep</code> et"
-        " <code>Bor_inc_resp</code>, pour enivron 65% des incidents, ces trois variables sont nulles "
-        "ce qui indique que l'arrondissement correspond à la caserne (déployée et responsable). De plus l'écart sur"
-        " le temps moyen d'intervention est supérieur à 5% pour deux de ces variables (<code>Bor_resp_rep</code> : 14%,"
-        " <code>Bor_inc_rep</code> : 10% et <code>Bor_inc_resp</code> : 4%).")
+      st.markdown("### Variables explicatives de propriété")
+      st.markdown(texte_justifie(
+        "Comme pour le type d'incident il existe deux variables descriptives : <code>'PropertyCategory'</code> et"
+        " <code>'HighPropertyType'</code> qui donnent une information plus ou moins détaillée sur le type de propriété.")
         , unsafe_allow_html=True)
-    st.markdown("<br>", unsafe_allow_html=True)
+      
+      # Choix de l'arbre affiché
+      var_liste = ['PropertyCategory','DetailedIncidentGroup']
+      # Menu déroulant pour un choix unique
+      var = st.selectbox("Choisissez une variable d'intérêt :", var_liste)
+      
+      # Création des colonnes
+      col1, col2 = st.columns(2)  # Crée deux colonnes de largeur égale
+      
+      # Affichage du premier graphique dans la première colonne
+      with col1:
+        fonctionsgraph.graphQuali_countPlot(df2023, var)
+    
+      # Affichage du deuxième graphique dans la deuxième colonne
+      with col2:
+        fonctionsgraph.graphQuali_meanPlot(df2023, var, rest=50)
+    
+      with st.expander(f"📌 Interprétation"):
+        st.markdown(texte_justifie(
+            "La distribution des temps est similaire selon le type de propriété. "
+            "La différence entre la moyenne la plus forte et la plus faible est de l'ordre de 10% "
+            "pour le total.<br>"
+            "Pour la variable qui détaille plus finement le type de propriété impacté par l'incident ;"
+            " chaque catégorie de HighPropertyType appartient à une seule catégorie de PropertyCategory."
+            " Il est donc cohérent de voir que plus de 60% des incidents ont lieu dans un immeuble d'habitation"
+            " (catégorie majoritaire parmi les bâtiments d'habitation).<br>"
+            "La différence entre la moyenne la plus forte et la plus faible est supérieur à 15% pour le temps total."
+            )
+            , unsafe_allow_html=True)
+        st.markdown("<br>", unsafe_allow_html=True)
+
+      st.markdown("### Distance")
+      st.markdown(texte_justifie(
+        "La distance est la seule variable explicative quantitative de notre jeu de données."
+        " Elle varie de 0,6 mètre à 18,8 kilomètres. Sa distribution est asymétrique à droite, 50%"
+        " des valeurs étant comprise entre 0.87 et 2,2 kilomètres.")
+        , unsafe_allow_html=True)
+      
+      df2023['distance_r']=np.round(df2023.distance,0)
+
+            # Création des colonnes
+      col1, col2 = st.columns(2)  # Crée deux colonnes de largeur égale
+
+      with col1:
+        fonctionsgraph.graph_countIncident(df2023, 'distance_r')
+    
+      # Affichage du deuxième graphique dans la deuxième colonne
+      with col2:
+        fonctionsgraph.graphQuali_pointplot(df2023, 'distance_r')
+
+      st.markdown("### RatioSC")
+      st.markdown(texte_justifie(
+        "")
+        , unsafe_allow_html=True)
+      
+      df2023['distance_r']=np.round(df2023.distance,0)
+
+      # Création des colonnes
+      col1, col2 = st.columns(2)  # Crée deux colonnes de largeur égale
+
+      with col1:
+        fonctionsgraph.graph_countIncident(df2023, 'ratioSC')
+    
+      # Affichage du deuxième graphique dans la deuxième colonne
+      with col2:
+        st.image("Donnees/Images/graph_distance.png",
+                  caption="Illustration du Gradient Boosting", use_container_width=True)
+      
+    
+
+
