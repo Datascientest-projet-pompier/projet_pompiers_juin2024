cba1eb32fb4c7997910de67acc4cda97de5ef35e
ChristelleTessier <christelle.tessier@eleve.ensai.fr>
2025-03-30 21:57:50 +0200

    sans lime

--- Streamlit/page_prediction_v2.py
+++ Streamlit/page_prediction_v2.py
@@ -416,7 +416,8 @@ def predictionv2():
             tab1, tab2 = st.tabs(["Expplicabilité avec Shap","Explicabilité avec Lime"])
 
             with tab2:
-                afficher_explication_lime(df_bilan,gb_model2)
+                #afficher_explication_lime(df_bilan,gb_model2)
+                st.write("temps")
 
             with tab1:
                 afficher_explication_shap(df_bilan)
